e0a29c6758462cd3b10df19ff44b708ba4abe8cf
nonso2002 <nonsoallison9@gmail.com>
2021-08-25 08:19:31 -0700

    first push

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    // var settings = {
+    //     "url": "localhost:3000/api/v1/vendors/view-meals",
+    //     "method": "GET",
+    //     "timeout": 0,
+    // };
+      
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
+
+    $.ajax({
+        url:"https://unboxng.herokuapp.com",
+        method:"GET",
+        async:false,
+        success:function(data){ 
+            console.log(data);
+        },       
+    });
+
+
+
+    
+    $(".pay_to_order").click(function(){
+        
+    })
+})
